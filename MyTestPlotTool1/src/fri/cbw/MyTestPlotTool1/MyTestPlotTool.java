aa0f7766ccbe96c177d8ce61c0a360cb74817fde
saso.petrovic@gmail.com <saso.petrovic@gmail.com@e92366fc-7d8e-e608-9467-dc0cd92998c9>
2012-12-03 15:57:53 +0000

    Uncommited data
    git-svn-id: http://cbw.googlecode.com/svn/trunk@12 e92366fc-7d8e-e608-9467-dc0cd92998c9

---
+++ MyTestPlotTool1/src/fri/cbw/MyTestPlotTool1/MyTestPlotTool.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fri.cbw.MyTestPlotTool1;
+
+import fri.cbw.GenericTool.AbstractPlotTool;
+
+/**
+ *
+ * @author Sašo
+ */
+public class MyTestPlotTool extends AbstractPlotTool{
+
+    @Override
+    public String getName() {
+        return "MyTestPlotTool";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "Sašo";
+    }
+
+    @Override
+    public String getTopComponentName() {
+        return "MyTestPlotToolTopComponent";
+    }
+    
+}
