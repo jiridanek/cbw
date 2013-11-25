49e69cc950ddbdd94e1bfb9896e7f18ca4e96278
saso.petrovic@gmail.com <saso.petrovic@gmail.com@e92366fc-7d8e-e608-9467-dc0cd92998c9>
2013-11-25 20:36:33 +0000

    Cleanup
    git-svn-id: http://cbw.googlecode.com/svn/trunk@37 e92366fc-7d8e-e608-9467-dc0cd92998c9

--- Stud-moduls/TauLeaping/TauLeaping/build/Debug/GNU-Linux-x86/main.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/GNU-Linux-x86/main.o: main.cpp tauLeaping.h utils.h
-
-tauLeaping.h:
-
-utils.h:
--- Stud-moduls/TauLeaping/TauLeaping/build/Debug/GNU-Linux-x86/tauLeaping.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/GNU-Linux-x86/tauLeaping.o: tauLeaping.cpp tauLeaping.h \
- utils.h
-
-tauLeaping.h:
-
-utils.h:
